3c2704bf3470227264cf78fccc99c1336955c097
tste0042 <161452963+tste0042@users.noreply.github.com>
2026-06-02 22:33:03 +1000

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "activity_ranking.json";
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+var vg_1 = "https://raw.githubusercontent.com/tste0042/FIT2179/refs/heads/main/charts/activity_ranking.json";
+vegaEmbed("#activity_ranking", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
