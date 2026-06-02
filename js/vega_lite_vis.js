29dbe18a118e6bf7aa45e8750d58838b6e33ce47
tste0042 <161452963+tste0042@users.noreply.github.com>
2026-06-02 23:02:46 +1000

    Update vega_lite_vis.js

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,4 +1,54 @@
-var vg_1 = "https://raw.githubusercontent.com/tste0042/FIT2179/refs/heads/main/charts/activity_ranking.json";
-vegaEmbed("#activity_ranking", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+var vg_1 = "charts/activity_ranking.json";
+vegaEmbed("#activity_ranking", vg_1, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_2 = "charts/running_by_age.json";
+vegaEmbed("#running_by_age", vg_2, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "charts/running_by_gender.json";
+vegaEmbed("#running_by_gender", vg_3, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "charts/running_year_comparison.json";
+vegaEmbed("#running_year_comparison", vg_4, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_5 = "charts/running_state_map.json";
+vegaEmbed("#running_state_map", vg_5, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "charts/parkrun_location_map.json";
+vegaEmbed("#parkrun_location_map", vg_6, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_7 = "charts/parkrun_by_state.json";
+vegaEmbed("#parkrun_by_state", vg_7, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_8 = "charts/parkrun_access.json";
+vegaEmbed("#parkrun_access", vg_8, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_9 = "charts/participation_vs_access.json";
+vegaEmbed("#participation_vs_access", vg_9, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_10 = "charts/city_temperature_heatmap.json";
+vegaEmbed("#city_temperature_heatmap", vg_10, {actions: false}).then(function(result) {
+
+}).catch(console.error);
+
+var vg_11 = "charts/city_running_conditions_summary.json";
+vegaEmbed("#city_running_conditions_summary", vg_11, {actions: false}).then(function(result) {
+ 
 }).catch(console.error);
